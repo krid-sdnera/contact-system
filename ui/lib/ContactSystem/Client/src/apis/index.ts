daf476cea36d17c339a25f6b08ae3d0b153e3ad6
Dirk Arends <dirk@arends.com.au>
2020-06-28 22:10:16 +1000

    Initial commit of generated client code

---
+++ ui/lib/ContactSystem/Client/src/apis/index.ts
@@ -0,0 +1,7 @@
+export * from './ContactsApi';
+export * from './GroupsApi';
+export * from './ListsApi';
+export * from './MembersApi';
+export * from './RolesApi';
+export * from './SectionsApi';
+export * from './SubsidiariesApi';
