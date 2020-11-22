c2264c76b93d1cfe6c553848fe722751ee9a7877
Dirk Arends <dirk@arends.com.au>
2020-11-22 12:31:51 +1100

    Add dark/light switch to the footer

--- ui/src/layouts/default.vue
+++ ui/src/layouts/default.vue
@@ -78,6 +78,38 @@
     </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-spacer />
+      <div>
+        <v-tooltip v-if="!$vuetify.theme.dark" bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              v-on="on"
+              color="info"
+              small
+              fab
+              @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+            >
+              <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
+            </v-btn>
+          </template>
+          <span>Dark Mode On</span>
+        </v-tooltip>
+
+        <v-tooltip v-else bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              v-on="on"
+              color="info"
+              small
+              fab
+              @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+            >
+              <v-icon color="yellow">mdi-white-balance-sunny</v-icon>
+            </v-btn>
+          </template>
+          <span>Dark Mode Off</span>
+        </v-tooltip>
+      </div>
     </v-footer>
   </v-app>
 </template>
