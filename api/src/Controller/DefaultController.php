bec6bd136e19f3da1b543bada2c78eaa9292fdb8
Dirk Arends <dirk@arends.com.au>
2020-12-09 21:11:56 +1100

    Clean up default controller

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -17,47 +17,7 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        $message = "Hello.";
-        return new Response($message);
-    }
-
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
-    {
-        $em = $this->getDoctrine()->getManager();
-        var_dump($request->request->get('_username'));
-        $username = $request->request->get('_username');
-        $password = $request->request->get('_password');
-
-        $user = new User();
-        $user->setUsername($username);
-        $user->setPassword($encoder->encodePassword($user, $password));
-        $em->persist($user);
-        $em->flush();
-
-        return new Response(sprintf('User %s successfully created', $user->getUsername()));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function updateToken($existingToken, $newToken)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $user = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->findOneBy(['authToken' => $existingToken]);
-
-        $res = ($user === null) ? ['Created'] : ['Updated'];
-        if (!$user) {
-            $user = new User();
-            $user->setUsername($newToken);
-            $user->setPassword($newToken);
-        }
-        $user->setAuthToken($newToken);
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-        return new JsonResponse($res);
+        $message = ["message" => "Hello. I don't think you're meant to be here"];
+        return new JsonResponse($message);
     }
 }
