11bf579c04cfe8656c6de159005354608573bd19
Dirk Arends <dirk@arends.com.au>
2021-01-02 23:58:21 +1100

    Update build scripts to point to OAS file

--- common/bin/build-client.sh
+++ common/bin/build-client.sh
@@ -7,4 +7,4 @@ rm -r ui/lib/ContactSystem/Client/*
 
 echo "> Generate Client using openapi-generator generate"
 yarn install
-yarn run openapi-generator generate -i build/contact-system-api.yaml -g typescript-fetch -o ui/lib/ContactSystem/Client --git-user-id contact-system --git-repo-id ui --additional-properties=npmRepository=contact-system,npmName=ui,supportsES6=true,typescriptThreePlus=true,withInterfaces=true,legacyDiscriminatorBehavior=false
+yarn run openapi-generator generate -i common/contact-system-api.yaml -g typescript-fetch -o ui/lib/ContactSystem/Client --git-user-id contact-system --git-repo-id ui --additional-properties=npmRepository=contact-system,npmName=ui,supportsES6=true,typescriptThreePlus=true,withInterfaces=true,legacyDiscriminatorBehavior=false
