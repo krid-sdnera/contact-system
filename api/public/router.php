19b2450dcb1d550cdfb6be183409b69a05af28ce
Dirk Arends <dirk@arends.com.au>
2021-05-05 21:18:56 +1000

    Allow periods in uris during local development

---
+++ api/public/router.php
@@ -0,0 +1,16 @@
+<?php
+// https://github.com/symfony/symfony/issues/26099
+/**
+ * This router is required if you are running the built in PHP web server
+ * as it attempts to route static files through the natural router(index).
+ */
+if (php_sapi_name() == 'cli-server') {
+    $info = parse_url($_SERVER['REQUEST_URI']);
+
+    if ($info !== false && file_exists(sprintf("./%s", $info['path']))) {
+        return false;
+    } else {
+        include_once "index.php";
+        return true;
+    }
+}
